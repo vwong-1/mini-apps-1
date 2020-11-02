e29dd8da693cf877340c42774fc73fd50a7f9d63
Victor Wong <victorw1992@gmail.com>
2020-11-02 15:25:49 -0800

    Added onChange events for all inputs for forms

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -7,17 +7,25 @@ class F1 extends React.Component {
       email: '',
       password: ''
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState({[name]: value});
   }
 
   render() {
     return (
       <form>
         <label>Name:</label>
-        <input type="text" id="name" name="name" required></input><br></br>
+        <input type="text" id="name" name="name" required onChange={this.handleInputChange} /><br />
         <label>Email:</label>
-        <input type="text" id="email" name="email" required></input><br></br>
+        <input type="text" id="email" name="email" required onChange={this.handleInputChange} /><br />
         <label>Password:</label>
-        <input type="password" id="password" name="password" required></input><br></br>
+        <input type="password" id="password" name="password" required onChange={this.handleInputChange} /><br />
         <button type="submit" onClick={this.props.formClick}>Next</button>
       </form>
     )
@@ -36,19 +44,27 @@ class F2 extends React.Component {
       zipcode: '',
       phone: ''
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState({[name]: value});
   }
 
   render() {
     return (
       <form>
         <label>Address</label><br></br>
-        <input type="text" id="addressl1" name="addressl1" placeholder="Address Line 1"></input><br></br>
-        <input type="text" id="addressl2" name="addressl2" placeholder="Address Line 2"></input><br></br>
-        <input type="text" id="city" name="city" placeholder="San Francisco"></input><a>,</a>
-        <input type="text" id="state" name="state" placeholder="CA"></input><a> </a>
-        <input type="text" id="zip" name="zip" placeholder="94103"></input><br></br>
+        <input type="text" id="addressl1" name="addressl1" placeholder="Address Line 1" onChange={this.handleInputChange} /><br />
+        <input type="text" id="addressl2" name="addressl2" placeholder="Address Line 2" onChange={this.handleInputChange} /><br />
+        <input type="text" id="city" name="city" placeholder="San Francisco" onChange={this.handleInputChange} /><a>,</a>
+        <input type="text" id="state" name="state" placeholder="CA" onChange={this.handleInputChange} /><a> </a>
+        <input type="text" id="zipcode" name="zipcode" placeholder="94103" onChange={this.handleInputChange} /><br />
         <label>Phone Number</label><br></br>
-        <input type="text" id="phone" name="phone" placeholder="111-123-1234"></input><br></br>
+        <input type="text" id="phone" name="phone" placeholder="111-123-1234" onChange={this.handleInputChange} /><br />
         <button type="submit" onClick={this.props.formClick}>Next</button>
       </form>
     )
@@ -66,22 +82,29 @@ class F3 extends React.Component {
       cvv: '',
       billingzip: ''
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  handleInputChange(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState({[name]: value});
+  }
 
   render() {
     return (
       <form>
-        <label>Card Number</label><br></br>
-        <input type="text" id="card" name="card" placeholder="1111-2222-3333-4444"></input><br></br>
-        <label>Exp Month</label><br></br>
-        <input type="text" id="expmonth" name="expmonth" placeholder="Jan"></input><br></br>
-        <label>Exp Year</label><br></br>
-        <input type="text" id="expyear" name="expyear" placeholder="2020"></input><br></br>
+        <label>Card Number</label><br />
+        <input type="text" id="card" name="card" placeholder="1111-2222-3333-4444" onChange={this.handleInputChange} /><br />
+        <label>Exp Month</label><br />
+        <input type="text" id="expmonth" name="expmonth" placeholder="Jan" onChange={this.handleInputChange} /><br />
+        <label>Exp Year</label><br />
+        <input type="text" id="expyear" name="expyear" placeholder="2020" onChange={this.handleInputChange} /><br />
         <label>CVV</label><br></br>
-        <input type="text" id="cvv" name="cvv" placeholder="123"></input><br></br>
-        <label>Billing Zip Code</label><br></br>
-        <input type="text" id="billingzip" name="billingzip" placeholder="00001"></input><br></br>
+        <input type="text" id="cvv" name="cvv" placeholder="123" onChange={this.handleInputChange} /><br />
+        <label>Billing Zip Code</label><br />
+        <input type="text" id="billingzip" name="billingzip" placeholder="00001" onChange={this.handleInputChange} /><br />
         <button type="submit" onClick={this.props.formClick}>Complete Checkout!</button>
       </form>
     )
