36ead1b2adc5903066268abb34829808a3c42944
Victor Wong <victorw1992@gmail.com>
2020-11-03 12:05:18 -0800

    Added click elements and rendering alternate X and O on click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,19 @@
+let count = 0;
 
+function player() {
+  if (count % 2) {
+    count++;
+    return 'O';
+  }
+  count++;
+  return 'X';
+}
 
+let rows = document.getElementById("game").rows;
+for (let i = 0; i < rows.length; i++) {
+  for (let j = 0; j < rows[i].cells.length; j++) {
+    rows[i].cells[j].addEventListener("click", (e) => {
+      e.target.innerHTML = player();
+    });
+  }
+}
