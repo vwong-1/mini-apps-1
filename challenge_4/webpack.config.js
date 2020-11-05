55eec2f3bdffea00615ed3ec02fda5ade5ff44ab
Victor Wong <victorw1992@gmail.com>
2020-11-04 22:32:01 -0800

    Configured webpack -- must use v4

---
+++ challenge_4/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const PUBLIC_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  watch: true,
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: PUBLIC_DIR,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: SRC_DIR,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    ]
+  }
+};
